eb03b7dac78596ed4f870a9bd29d042bbe9bf4b7
Combi2k2 <40012077+Combi2k2@users.noreply.github.com>
2021-03-30 23:51:43 +0700

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -1,59 +1,61 @@
-import numpy as np
+import numpy as np 
 import matplotlib.pyplot as plt
-import math
+from scipy.spatial.distance import cdist
 
 from matplotlib.animation import FuncAnimation
 
-X = np.random.rand(20,2)
-one  = np.ones((X.shape[0],1))
-Xbar = np.concatenate((one,X),axis = 1)
+np.random.seed(2)
 
-x0, y0 = (X.T)[0][:10], (X.T)[1][:10]   #belong to red class
-x1, y1 = (X.T)[0][10:], (X.T)[1][10:]   #belong to blue class
+means = [[2, 2], [4, 2]]
+cov = [[.3, .2], [.2, .3]]
+N = 10
+X0 = np.random.multivariate_normal(means[0], cov, N).T
+X1 = np.random.multivariate_normal(means[1], cov, N).T
 
-for i in range(10):
-    if (y0[i] < x0[i]): x0[i], y0[i] = y0[i], x0[i]
-    if (y1[i] > x1[i]): x1[i], y1[i] = y1[i], x1[i]
+X = np.concatenate((X0, X1), axis = 1)
+y = np.concatenate((np.ones((1, N)), -1*np.ones((1, N))), axis = 1)
+# Xbar 
+X = np.concatenate((np.ones((1, 2*N)), X), axis = 0)
 
-w = [1,1,-1]
+def h(w, x):    
+    return np.sign(np.dot(w.T, x))
 
-def animate(i):
-    global w
+def has_converged(X, y, w):    
+    return np.array_equal(h(w, X), y) 
 
-    misclassified = []
+N = X.shape[1]
+d = X.shape[0]
 
-    for i in range(10):
-        v = x0[i] * w[0] + y0[i] * w[1] + w[2]
-        if (v < 0):
-            misclassified.append([ x0[i],  y0[i],  1])
-    
-    for i in range(10):
-        v = x1[i] * w[0] + y1[i] * w[1] + w[2]
-        if (v >= 0):
-            misclassified.append([-x1[i], -y1[i], -1])
+w = [np.random.randn(d, 1)]
+mispoint = []
 
-    if (len(misclassified) > 0):
-        index = np.random.randint(0,len(misclassified))
+def perceptron(i):
+    mix_id = np.random.permutation(N)
 
-        w[0] = w[0] + misclassified[index][0]
-        w[1] = w[1] + misclassified[index][1]
-        w[2] = w[2] + misclassified[index][2]
+    for i in range(N):
+        xi = X[:, mix_id[i]].reshape(d, 1)
+        yi = y[0, mix_id[i]]
+
+        if (h(w[-1], xi)[0] != yi): #misclassified point
+            mis_points.append(mix_id[i])
+            w_new = w[-1] + yi * xi
+            w.append(w_new)
     
     wx = np.linspace(-5, 5, 100)
-    wy = -(w[0] * wx + w[2]) / w[1]
+    wy = -(w[-1][0] * wx + w[-1][2]) / w[-1][1]
 
     plt.cla()
-    plt.plot(wx, wy, '-r', label = '{}X + {}Y + {} = 0'.format(w[0], w[1], w[2]))
+    plt.plot(wx, wy, '-r', label = '{}X + {}Y + {} = 0'.format(w[-1][0], w[-1][1], w[-1][2]))
     #plt.xlabel('x1')
     #plt.ylabel('x2')
     plt.legend(loc = 'upper left')
 
-    plt.plot(x0, y0, 'ro')
-    plt.plot(x1, y1, 'bs')
+    plt.plot((X0.T)[0], (X0.T)[1], 'ro')
+    plt.plot((X1.T)[0], (X1.T)[1], 'bs')
 
     plt.axis([0, 1, 0 ,1])
 
-ani = FuncAnimation(plt.gcf(),animate, interval = 500)
+ani = FuncAnimation(plt.gcf(),perceptron, interval = 500)
 
 plt.tight_layout()
 plt.show()
